89f6b093cdfa65bdccc94b98376108d6a6bb6cf8
Arindam Batabyal <abatabyal@gmail.com>
2019-11-06 01:23:13 +0530

    new

--- algorithms/sorting/insertionsort.py
+++ algorithms/sorting/insertionsort.py
@@ -1,15 +1,39 @@
 def insertion_sort(arr):
-
     for i in range(1, len(arr)):
         j = i -1
-
         key = arr[i]
         while j >= 0 and key < arr[j]:
             arr[j+1] = arr[j]
             j -= 1
         arr[j+1] = key
 
-arr = [12, 11, 13, 5, 6]
-insertion_sort(arr)
+def binary_search(arr, value, start, end):
+    if start == end:
+        if arr[start] > value:
+            return start
+        else:
+            return start + 1
+
+    if start > end:
+        return start
+
+    mid = (start + end) / 2
+    if arr[mid] < value:
+        return binary_search(arr, value, mid+1, end)
+    elif arr[mid] > value:
+        return binary_search(arr, value, start, mid-1)
+    else:
+        return mid
+
+def binary_insert_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = binary_search(arr, key, 0, i-1)
+        arr = arr[:j] + key + arr[j:i] + arr[i+1:]
+    return arr
+arr = [5,2,4, 6, 1, 3]
+# insertion_sort(arr)
+binary_insert_sort(arr)
 for i in range(len(arr)):
-    print (arr[i])
+    print (arr[i], end=',')
+
